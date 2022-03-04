a947b0ba6518971aae400a0312c71aa12a0d9438
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-03-04 23:17:41 +0300

    Add Telegram.Domain

---
+++ TelegramBot.Domain/Models/TimeTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TelegramBot.Domain.Models
+{
+   public  class TimeTable
+    {
+        public int Id { get; set; }
+        public string LessonsOfTheDay { get; set; }
+        public DayOfWeek Week { get; set; }
+        public EvenWeek EvenWeek { get; set; }
+        public string Group { get; set; }
+
+        public TimeTable(string lessonsOfTheDay, DayOfWeek week, EvenWeek evenWeek)
+        {
+            LessonsOfTheDay = lessonsOfTheDay;
+            Week = week;
+            EvenWeek = evenWeek;
+        }
+
+        public TimeTable()
+        {
+
+        }
+       
+    }
+   public enum EvenWeek:byte
+   {
+       Even=1,
+       NotEven=2
+   }
+}
---
+++ TelegramBot.Domain/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TelegramBot.Domain.Models
+{
+   public  class User
+    {
+        public int Id { get; set; }
+        public long IdChat { get; set; }
+        public string NameGroup { get; set; }
+        public IEnumerable<TimeTable> TimeTables { get; set; }
+
+        public User(long idChat,string nameGroup)
+        {
+            IdChat = idChat;
+            NameGroup = nameGroup;
+        }
+
+        public User(long id)
+        {
+
+        }
+        
+    }
+}
