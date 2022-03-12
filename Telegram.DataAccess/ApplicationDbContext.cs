7271f71bb7b567af5a5c5208a9971f041dc4bb7f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-03-12 16:29:13 +0300

    Add DataAccess

---
+++ Telegram.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TelegramBot.Domain.Models;
+
+namespace Telegram.DataAccess
+{
+     public class ApplicationDbContext:DbContext
+    {
+           public DbSet<TimeTable> TimeTables { get; set; }
+            public DbSet<User> Users { get; set; }
+            public DbSet<Subscription> Subscriptions { get; set; }
+            public DbSet<TelegramBot.Domain.Models.Group> Groups { get; set; }
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> optionBuilder) : base(optionBuilder)
+            {
+
+            }
+        
+    }
+}
---
+++ Telegram.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Telegram.DataAccess
+{
+    public class Class1
+    {
+    }
+}
